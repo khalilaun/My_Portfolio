b91f3b7a49fa275ae428bca9f18c94f03566d878
Khalil Aun <143788071+khalilaun@users.noreply.github.com>
2026-07-27 04:24:54 -0400

    Fix profile image references

--- components/About.jsx
+++ components/About.jsx
@@ -34,7 +34,7 @@ const About = ({isDarkMode}) => {
             whileInView={{opacity: 1, scale: 1}}
             transition={{duration: 0.6}}
             className='w-64 sm:w-80 rounded-3xl max-w-none'>
-                <Image src={assets.profile_image} alt='user' className='w-full rounded-3xl'/>
+                <Image src={assets.profile_photo} alt='user' className='w-full rounded-3xl'/>
             </motion.div>
             <motion.div 
             initial={{opacity: 0}}
